15cdd9a4ad7609e962e3052cf506d84f465a38ff
Eugene Kwon <ekwon86@gmail.com>
2019-01-10 08:04:05 -0800

    working on adding redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 class App extends Component {
